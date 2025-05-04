a2b1ca667adac507d5dc988c2aa5dfd5135f07e3
EpicTeddy101 <104555931@student.swin.edu.au>
2025-05-04 21:56:19 +1000

    Add members routes with login requirement and member listing

--- routes/members.py
+++ routes/members.py
@@ -1,1 +1,12 @@
 # Start of the Members Routes
+from flask import Blueprint, render_template
+from flask_login import login_required
+from models.member import Member
+
+member_routes = Blueprint('members', __name__, url_prefix='/members')
+
+@member_routes.route('/')
+@login_required
+def index():
+    members = Member.query.all()
+    return render_template('members/index.html', members=members)
